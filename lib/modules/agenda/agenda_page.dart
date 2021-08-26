e20451968a3913377ed720008b1a88d683f54179
Luis Covelo <luis.covelo@hotmail.com>
2021-08-26 16:31:06 -0300

    adicionado tela de agenda - nao está funcional.

--- lib/modules/agenda/agenda_page.dart
+++ lib/modules/agenda/agenda_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:tutor/shared/widgets/title_page_widget/title_page_widget.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 class AgendaPage extends StatefulWidget {
   const AgendaPage({Key? key}) : super(key: key);
@@ -9,16 +10,26 @@ class AgendaPage extends StatefulWidget {
 }
 
 class _AgendaPageState extends State<AgendaPage> {
+  final kToday = DateTime.now();
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      key: UniqueKey(),
-      child: Column(
-        children: [
-          TitlePageWidget(
-            title: "Agenda",
-          ),
-        ],
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(64),
+        child: TitlePageWidget(
+          title: "Minha agenda",
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: TableCalendar(
+          locale: 'pt_BR',
+          firstDay: DateTime.now(),
+          focusedDay: DateTime.now(),
+          lastDay: DateTime.now(),
+          calendarFormat: CalendarFormat.month,
+        ),
       ),
     );
   }
